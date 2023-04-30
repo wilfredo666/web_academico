88b3e6fa72f76b0b49adf49e88b724cffb966a54
amaru <amaru.el.trueno@gmail.com>
2023-04-30 18:31:48 -0400

    CONEXION

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -5,16 +5,21 @@ class Conexion
     /* CONEXION POR PDO */
     static public function conectar()
     {
-        /* PARA TRABAJAR DE MANERA LOCAL */
-        $host = "localhost";
+        /* =============================
+         PARA TRABAJAR DE MANERA LOCAL 
+         =================================*/
+        /* $host = "localhost";
         $db = "web_academico";
         $userDB = "root";
-        $passDB = "";
+        $passDB = ""; */
         
-       /*  $host = "localhost";
+        /* =============================
+         PARA CONECTAR CON EL HOSTING
+         =================================*/
+        $host = "localhost";
         $db = "u263048258_web_academico";
         $userDB = "u263048258_web_academico";
-        $passDB = "Administrador123"; */
+        $passDB = "Administrador123";
 
         $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);
         $link->exec("set names utf8");
